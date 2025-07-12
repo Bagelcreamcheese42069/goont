b6106abab6c8269288dc94c53c8fdc4218ebc24f
Bagelcreamcheese42069 <bagelisbasil@gmail.com>
2025-07-12 16:14:19 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import babygoont from './Assets/babygoont.webp';
 function App() {
   return (
     <div className="App">
+      <button> CLICK IF ZORTING IT!!!!!! </button>
       <h1>I'm zorting it!!!!</h1>
       <img src ={babygoont} alt="baby goont" />
     </div>
