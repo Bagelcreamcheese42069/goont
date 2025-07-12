4aac04a637fa6e6cf70fa0bfc868153b045ab51a
Bagelcreamcheese42069 <bagelisbasil@gmail.com>
2025-07-12 15:03:09 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import babygoont from './Assets/babygoont.webp';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>If groot was stroking his branch up and down all day do you think he'd say i am goont?</h1>
+      <img src ={babygoont} alt="baby goont" />
+    </div>
+    );
+}
+
+export default App;
