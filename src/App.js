98026e49621f3fde2e4a63b46f9d9fe459577670
Bagelcreamcheese42069 <bagelisbasil@gmail.com>
2025-07-12 15:13:16 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import babygoont from './Assets/babygoont.webp';
 function App() {
   return (
     <div className="App">
-      <h1>If garot was stroking his branch up and down all day do you think he'd say i am goont?</h1>
+      <h1>If groot was stroking his branch up and down all day do you think he'd say i am goont?</h1>
       <img src ={babygoont} alt="baby goont" />
     </div>
     );
